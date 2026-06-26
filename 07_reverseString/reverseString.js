2ad39b8fdc1a7a2a080fc222f6fe2ccd3fe9a1dc
LargeWeight <kjgolisao@gmail.com>
2026-06-26 10:33:08 +0800

    finished solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 };
+
+console.log(reverseString("Miss na miss ko na ang baby ko"))
 
 // Do not edit below this line
 module.exports = reverseString;
