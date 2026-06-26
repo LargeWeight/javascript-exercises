6189be792e99f6c3861eecc82c897a6ff36d6e67
LargeWeight <kjgolisao@gmail.com>
2026-06-26 09:54:53 +0800

    finished activity

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    outputString = "";
+    for (let i = 0; i < num; i++) {
+        outputString += string;
+    }
+    return outputString;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
